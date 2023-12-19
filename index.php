2676a6ae0c17ca5d3295ce05841636cc9131d1e6
jaylingers <jomariejaybatingal@gmail.com>
2023-12-19 21:36:55 +0800

    added logo

--- index.php
+++ index.php
@@ -60,6 +60,7 @@ if (isset($_SESSION['user_type'])) {
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="assets/img/mabes.png" />
     <title>GABI OFFICIAL WEBSITE</title>
     <link rel="stylesheet" href="assets/css/main.css"/>
     <link rel="stylesheet" href="assets/css/media.css"/>
